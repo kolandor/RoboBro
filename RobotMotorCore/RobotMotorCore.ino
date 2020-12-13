6119f106c65c5142ba7debdea9b97ee55759d667
alexey.anipchenko <anipchenko@mindwave.studio>
2020-12-13 14:49:22 +0200

    Create Driver control function

---
+++ RobotMotorCore/RobotMotorCore.ino
@@ -0,0 +1,44 @@
+//direction of rotation of the wheel
+#define FORWARD 1
+#define BACKWARD 0
+
+//possibility of engines during the dance (test drive)
+#define DANCE_PWR 150
+
+//---- you can change the values ​​of these pins for the convenience of connecting your motor driver ----
+
+//contacts responsible for the direction of rotation of the wheel.
+#define LEFT_FORWARD 8
+#define LEFT_BACKWARD 7
+#define RIGHT_FORWARD 5
+#define RIGHT_BACKWARD 4
+
+//PWM Arduino contacts responsible for motor power
+#define POWER_RIGHT 3
+#define POWER_LEFT 9
+
+void setup()
+{
+  //initialize the pins that control the driver
+  pinMode(LEFT_FORWARD, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT);
+  pinMode(LEFT_BACKWARD, OUTPUT);
+  pinMode(RIGHT_BACKWARD, OUTPUT);
+}
+
+void loop()
+{
+
+}
+
+//Driver control function
+void Move(byte &leftState, byte &leftPower, byte &rightState, byte &rightPower)
+{
+    analogWrite(POWER_LEFT, leftPower);
+    digitalWrite(LEFT_FORWARD, leftState);   
+    digitalWrite(LEFT_BACKWARD, !leftState); 
+
+    analogWrite(POWER_RIGHT, rightPower);
+    digitalWrite(RIGHT_FORWARD, rightState);
+    digitalWrite(RIGHT_BACKWARD, !rightState);
+}
