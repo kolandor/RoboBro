2b253ad5965adb3faf18d303a054da6d045fb8a7
alexey.anipchenko <anipchenko@mindwave.studio>
2020-12-13 14:41:25 +0200

    Add motor driver defines

--- RobotMotorCore/MotorCore/MotorCore.ino
+++ RobotMotorCore/MotorCore/MotorCore.ino
@@ -1,3 +1,21 @@
+//direction of rotation of the wheel
+#define FORWARD 1
+#define BACKWARD 0
+
+//possibility of engines during the dance (test drive)
+#define DANCE_PWR 150
+
+//---- you can change the values ​​of these pins for the convenience of connecting your motor driver ----
+
+//contacts responsible for the direction of rotation of the wheel.
+#define LEFT_FORWARD 8
+#define LEFT_BACKWARD 7
+#define RIGHT_FORWARD 5
+#define RIGHT_BACKWARD 4
+
+//PWM Arduino contacts responsible for motor power
+#define POWER_RIGHT 3
+#define POWER_LEFT 9
 void setup()
 {
   
